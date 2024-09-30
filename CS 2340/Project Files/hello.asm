a452ff5184f2f5baf6a2b79e053be98a4f960fb3
jacobD44 <jacobadehoyos@gmail.com>
2024-09-29 19:46:56 -0500

    Second attempt trying to commit changes

--- CS 2340/Project Files/hello.asm
+++ CS 2340/Project Files/hello.asm
@@ -1,5 +1,6 @@
 .data
-hello:      .asciiz "Hello, World!"
+hello:      .asciiz "Hello, World!\n"
+hello2: .asciiz "Hello again"
 
 .text
 main:
@@ -7,6 +8,11 @@ main:
     li $v0, 4           # Load the syscall for print string
     la $a0, hello       # Load the address of the string to print
     syscall             # Make the syscall
+    
+    # Print the "Hello again" string
+    li $v0, 4			# get ready to print a string
+    la $a0, hello2		# set address of $a0 to hello2
+    syscall			# print string contained in hello2
 
     # Exit the program
     li $v0, 10          # Load the syscall for exit
